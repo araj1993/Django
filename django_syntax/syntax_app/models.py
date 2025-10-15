3410ebd4a59beceb1eb0979196a37263209d575e
araj1993 <araj20sep@gmail.com>
2025-10-15 10:58:22 +0530

    db migration

--- django_syntax/syntax_app/models.py
+++ django_syntax/syntax_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    student_name = models.CharField(max_length=100)
+    student_age = models.IntegerField()
+    student_email = models.EmailField()
+    student_join = models.DateTimeField(auto_now_add=True)
+    student_reg_num = models.IntegerField()
+
+    
